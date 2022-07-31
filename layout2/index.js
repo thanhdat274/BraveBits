2d06658f7a938da91ff48f9765ba55eccadccb2e
thanhdat274 <nguyenthanhdat27042002@gmail.com>
2022-07-31 18:49:01 +0700

    update final layout2

---
+++ layout2/index.js
@@ -0,0 +1,12 @@
+let header = document.getElementById('header')
+let btnMenu = document.getElementById('btn-menu')
+btnMenu.onclick = function(){
+    console.log(header.clientHeight);
+    var isOpen = header.style.display === 'block'
+    if(isOpen){
+        header.style.display = null 
+    }else{
+        header.style.display = 'block' 
+    }
+}
+console.log(btnMenu);
