97d0d8a22982a482deeb2fea5652421766dcc77f
thanhdat274 <nguyenthanhdat27042002@gmail.com>
2022-08-15 14:31:59 +0700

    update responsive layout 3 màn hình 480 -> 640

---
+++ Layout3/js/index3.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('btn-menu')
+const menu = document.querySelector('.menu-bottom')
+btn.addEventListener('click', function(){
+    menu.classList.toggle("show")
+    console.log("ahihihi");
+} )
