a722839ee36ad9152158af64e48eb746c19f2acc
thanhdat274 <nguyenthanhdat27042002@gmail.com>
2022-08-22 17:52:59 +0700

    update

--- PageFly/js/pagefly.js
+++ PageFly/js/pagefly.js
@@ -1,8 +1,20 @@
-$(document).ready(function () {
-  $('.box-section2-center').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
-  });
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  autoPlay: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
+
+
+const btn = document.getElementById('btn-menu')
+const menu = document.querySelector('.menu')
+btn.addEventListener('click', function () {
+    menu.classList.toggle("show")
+    console.log("ahihihi");
+})
