076e8b855e2a020641dca0e1eadbb3b7963585b9
thanhdat274 <nguyenthanhdat27042002@gmail.com>
2022-08-23 16:44:04 +0700

    fix lỗi phông chữ với slidéhow

--- PageFly/js/pagefly.js
+++ PageFly/js/pagefly.js
@@ -1,20 +1,21 @@
 const swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
-  slidesPerGroup: 3,
-  loop: true,
-  autoPlay: true,
-  loopFillGroupWithBlank: true,
+  
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  }
 });
 
 
 const btn = document.getElementById('btn-menu')
 const menu = document.querySelector('.menu')
 btn.addEventListener('click', function () {
-    menu.classList.toggle("show")
-    console.log("ahihihi");
+  menu.classList.toggle("show")
+  console.log("ahihihi");
 })
