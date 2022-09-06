1e5e4aa485835c763833a782c15107c2a1d4b8fa
thanhdat274 <nguyenthanhdat27042002@gmail.com>
2022-09-06 09:07:23 +0700

    update

---
+++ DECARLO/js/history.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('btn-menu')
+const menu = document.querySelector('.menu-list')
+btn.addEventListener('click', function () {
+  menu.classList.toggle("show")
+  console.log("ahihihi");
+})
